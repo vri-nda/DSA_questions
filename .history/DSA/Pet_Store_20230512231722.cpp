d240cbb0b8c9bd4b1539b4933859e48ef31e0e09
vri-nda <vsvrindasharma2@gmail.com>
2023-05-12 23:17:47 +0530

    Pet Store

---
+++ .history/DSA/Pet_Store_20230512231722.cpp
@@ -0,0 +1,48 @@
+// codechef
+
+// Problem
+// Alice and Bob went to a pet store. There are N animals in the store where the ith animal is of type 
+// Ai. Alice decides to buy some of these 
+// N animals. Bob decides that he will buy all the animals left in the store after Alice has made the purchase.
+
+// Find out whether it is possible that Alice and Bob end up with exactly same multiset of animals.
+
+
+// ---finding whether the frequency of elements in array is even(yes) or odd(no)--------
+
+
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    int a[n];
+	    int ans=0;
+	    map <int, int> mp;
+	    for(int i=0; i<n; i++){
+	        cin>>a[i];
+	        mp[a[i]]++;
+	       // ans=ans^a[i];
+	    }
+	    for(auto x:mp){
+	        if(x.second%2!=0){
+	            ans++;
+	            break;
+	        }
+	    }
+	    if(ans>0){
+	        cout<<"NO"<<endl;
+	    }
+	    else{
+	        cout<<"YES"<<endl;
+	    }
+	}
+	return 0;
+}
+
