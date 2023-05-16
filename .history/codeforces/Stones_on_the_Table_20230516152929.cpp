091d8313ff010b4f21aa630119d85371baa9bfe3
vri-nda <vsvrindasharma2@gmail.com>
2023-05-16 15:29:38 +0530

    Stones on the Table

---
+++ .history/codeforces/Stones_on_the_Table_20230516152929.cpp
@@ -0,0 +1,25 @@
+
+// Stones on the Table
+// There are n stones on the table in a row, each of them can be red, green or blue. Count the minimum number of
+//  stones to take from the table so that any two neighboring stones had different colors.
+//  Stones in a row are considered neighboring if there are no other stones between them.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int count=0;
+    for(int i=0; i<n; i++){
+        if(s[i] == s[i+1]){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+	return 0;
+}
